239eb9c29279bb91b19d3631accc69e2946a0c9d
Naruse-Kazuki <kuroraido@icloud.com>
2020-05-05 19:06:05 +0900

    Add_Container3

--- lib/main.dart
+++ lib/main.dart
@@ -21,14 +21,20 @@ class MyApp extends StatelessWidget {
               Container(
                 height: 100,
                 width: 100,
-                color: Colors.red,
+                color: Colors.pink,
                 child: Text('Container 1'),
               ),
               Container(
                 height: 100,
                 width: 100,
-                color: Colors.purple,
+                color: Colors.cyan,
                 child: Text('Container 2'),
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.green,
+                child: Text('Container 3'),
               ),
             ],
           ),
